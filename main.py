e358c11569cee3538cd81a6548764d7936b7a736
eleve.shi <eleve.shi@gmail.com>
2025-04-13 18:40:23 +0800

    add: alert msg

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ tab1, tab2, tab3 = st.tabs(["二分查找", "Dog", "Owl"])
 with tab1:
     b_search = BinarySearch()
     b_search.frontpage()
+
+with tab2:
+    st.write("to be continue...")
+
+with tab3:
+    st.write("to be continue...")
